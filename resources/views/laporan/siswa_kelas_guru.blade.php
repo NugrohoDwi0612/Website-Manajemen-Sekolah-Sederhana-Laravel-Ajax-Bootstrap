8f60ae971c72de4b9aeb774455e9a07027af911c
Nugroho Dwi <nugroho1116@gmail.com>
2025-04-23 15:57:05 +0700

    revisi

--- resources/views/laporan/siswa_kelas_guru.blade.php
+++ resources/views/laporan/siswa_kelas_guru.blade.php
@@ -18,11 +18,6 @@
 @endsection
 
 @push('scripts')
-<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/dataTables.bootstrap5.min.css">
-<script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
 <script>
     $(function() {
         $('#siswaKelasGuruTable').DataTable({
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,6 +54,12 @@
         @yield('content')
     </main>
 
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/dataTables.bootstrap5.min.css">
+    <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
+
     @stack('scripts')
 
 </body>
